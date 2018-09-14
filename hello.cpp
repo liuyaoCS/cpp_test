1b6a43cccd1c82ff6085675f1eb85598b69f3ac1
liuyao-s <liuyao-s@360.cn>
2018-09-14 15:54:29 +0800

    init

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "hello, vscode from c++"<<endl;
+    int a=1;
+    a=a+2;
+    cin>> a; // keep termimal not exit
+    return 0;
+}
+
